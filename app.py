8830bc24ffe9b635b75b94e3d1337ac98feeeac6
Rishabh Bhasin <rishabhgndu2023@gmail.com>
2025-11-17 10:34:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,9 +134,9 @@ if page == "🌐 Home":
             return None
 
     # Load datasets safely
-    diabetes_df = load_csv("Dataset/diabetes.csv")
-    kidney_df = load_csv("Dataset/kidney_disease.csv")
-    heart_df = load_csv("Dataset/heart.csv")
+    diabetes_df = load_csv("Datasets/diabetes.csv")
+    kidney_df = load_csv("Datasets/kidney_disease.csv")
+    heart_df = load_csv("Datasets/heart.csv")
 
     colA, colB, colC = st.columns(3)
 
@@ -497,4 +497,5 @@ st.sidebar.markdown("\n\n")
 st.sidebar.markdown("\n\n")
 
 st.sidebar.subheader("Thank you for choosing MediPredict❤️— your intelligent partner for early disease detection.")
+
 #### ---- ####
